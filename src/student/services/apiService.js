d649a8649412cd2099f31fc412e95590d48dea34
hariiikrishnan <therihari36@gmail.com>
2026-02-12 21:18:03 +0530

    checking prod

--- src/student/services/apiService.js
+++ src/student/services/apiService.js
@@ -1,7 +1,7 @@
 // src/services/apiService.js
 
-// const BASE_URL = "http://10.208.14.196:3000";
-const BASE_URL = "/api";
+const BASE_URL = "http://10.208.14.196:3000";
+// const BASE_URL = "/api";
 // const BASE_URL = "https://qr-attendance-backend-pr5h.onrender.com";
 
 const HEADERS = {
